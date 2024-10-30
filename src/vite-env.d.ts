d3cde7320b20313e24b46fb59ef545c63d272281
Ilia.Kalendarev <kilayser@gmail.com>
2024-10-30 17:04:48 +0300

    svg setting

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg"
+declare module "*.svg" {
+    import React from "react";
+    const SVG: React.FC<React.SVGProps<SVGSVGElement>>;
+    export default SVG;
+}
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,18 +1,17 @@
 
 import cls from "./Navbar.module.scss";
-import {useTranslation} from "react-i18next";
 import {classNames} from "../../../shared/lib/classNames";
-import {Button} from "../../../shared/ui/Button/Button";
-import {ButtonOutlineStyle} from "../../../shared/ui/Button/ButtonStyleType";
+import {Button, ButtonTheme} from "../../../shared/ui/Button/Button";
 import {useCallback, useState} from "react";
 import {LoginModal} from "../../../features/AuthByEmail/ui/LoginModal/LoginModal";
+
+import LoginIcon from '../../../shared/icons/login.svg?react';
 
 interface NavbarProps {
     className?: string;
 }
 
 export const Navbar = ({className = ""}: NavbarProps) => {
-    const {t} = useTranslation();
 
     const [isAuthModal, setIsAuthModal] = useState(false);
 
@@ -27,11 +26,11 @@ export const Navbar = ({className = ""}: NavbarProps) => {
     return (
         <nav className={classNames(cls.Navbar, {}, [className])}>
             <Button
-                className={classNames(ButtonOutlineStyle.SECONDARY, {}, [cls.links])}
+                className={classNames(ButtonTheme.CLEAR, {}, [cls.links])}
                 onClick={onShowModal}
 
             >
-                {t("Войти")}
+                <LoginIcon/>
             </Button>
             <LoginModal
                 isOpen={isAuthModal}
